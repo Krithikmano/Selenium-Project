5b3d8d945a095db154d58440bd0049f46d364470
Karthik M <krithikmano@gmail.com>
2017-06-29 22:20:48 -0500

    added all the files

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,12 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={"src/test/java/features"})
+//@CucumberOptions(features="features",glue={"stepDefination"})
+public class TestRunner {
+}
+
